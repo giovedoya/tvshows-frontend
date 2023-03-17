152a1a67dcff15787e5e988d1a236554225e6761
Giovanni Bedoya <giovedoya@gmail.com>
2023-03-17 06:42:10 +0100

    'Iteration 1: navigation'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function Navbar() {
   return (
     <div>
-      {/* ITERATION 1: Should use react-router-dom NavLinks to browse through the different pages */}
+      <ul>
+        <li><NavLink className="nav_link" to='/'>Home</NavLink></li>
+        <li><NavLink className="nav_link" to='/new'>New Show</NavLink></li>
+        <li><NavLink className="nav_link" to='/'>Go back</NavLink></li>
+      </ul>
     </div>
   )
 }
